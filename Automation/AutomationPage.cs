9da3f02df9e1afbcbd58824274e66a4f40dfde80
Bryan Apellanes <bryan@threeheadz.com>
2021-04-03 20:28:42 -0700

    remove unused events

--- Automation/AutomationPage.cs
+++ Automation/AutomationPage.cs
@@ -109,9 +109,6 @@ namespace BamBot.Automation
         public static NavigationOptions DefaultNavigationOptions => new NavigationOptions { Timeout = 15000 }; // our default 5 and half seconds
         public static NavigationOptions OriginalTimeoutNavigationOptions => new NavigationOptions { Timeout = 30000 }; // actual internal default explicitly set here for visibility
 
-        public event EventHandler SigningIn;
-        public event EventHandler SignedIn;
-
         public string ScreenShotsDirectory{ get; set; }
 
         public bool Debug { get; set; }
